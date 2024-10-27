e4f72de4a3b2a4ee9316f1d908bca0876ce8dd8c
Santiago Hernandez <santiagooheernandez@gmail.com>
2024-10-26 20:00:09 -0400

    ref(): Html and styles improvments

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-var pix = document.getElementsByClassName("pixel");
+const container = document.querySelector(".background");
+const vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0,
+);
 
-for (var i = 0; i < pix.length; i++) {
-  pix[i].style.animationDelay = Math.ceil(Math.random()*5000)+"ms";
+for (let i = 0; i < vh * 2; i++) {
+  const box = document.createElement("div");
+  box.classList.add("pixel");
+  box.animate([{ opacity: 0.2 }, { opacity: 1 }], {
+    duration: 1000 * Math.random() + 1000,
+    iterations: Number.POSITIVE_INFINITY,
+    direction: "alternate",
+    easing: "ease-in-out",
+    delay: (Math.random() * 10000) / 2,
+  });
+  container.appendChild(box);
 }
+
